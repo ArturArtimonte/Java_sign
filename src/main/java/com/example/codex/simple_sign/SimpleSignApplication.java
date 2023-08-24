722baf53c86b7a07676f2a9283695b279104f798
ArturArtimonte <artur.artimonte@gmail.com>
2023-08-24 20:46:43 -0300

    first commit

---
+++ src/main/java/com/example/codex/simple_sign/SimpleSignApplication.java
@@ -0,0 +1,13 @@
+package com.example.codex.simple_sign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codex/simple_sign/SimpleSignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codex.simple_sign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
